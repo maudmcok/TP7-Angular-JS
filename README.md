c3a30e9b77e2201676a4fb6a5d7df7733954d04c
Maud <maudmcok001@gmail.com>
2018-04-02 09:53:46 +0200

    Modal base ok with #popeye
     Popeye  issimple modal library for AngularJS 
    link:https://pathgather.github.io/popeye/

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ version 0.16.0.
 
 ## Build & development
 
+Instal `bower install angular-popeye` from modal form
+
 Run `grunt` for building and `grunt serve` for preview.
 
 ## Testing
 
 Running `grunt test` will run the unit tests with karma.
+
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,26 @@
  * Controller of the tp7AngularJsApp
  */
 var tpapp = angular.module('tp7AngularJsApp');
+
 tpapp.factory('kami', ['$resource', function($resource) {
-  var path ;
-  var addPath = function(msg){
-    path =$resource('/rest/home/'),{
-      charge: {method:'GET'}
-     };
-    console.log('past her');
-  };
-  return {
-    setPath: function(msg){
-    addPath(msg);
-    },
-    getpath: function(){
-      return path ;
-    }
-  };
+    var path ;
+    var addPath = function(msg){
+      path =$resource('/rest/home/');
+      console.log('past her');
+    };
+    return {
+      setPath: function(msg){
+      addPath(msg);
+      },
+      getpath: function(){
+        return path ;
+      }
+    };
 }]);
 
-tpapp.controller('MainCtrl', function ($scope,kami,$http ) {
+tpapp.controller('MainCtrl', function ($scope,Popeye,kami,$http ) {
+
     console.log(" **** ");
-
-
-
 
     kami.setPath('home');
     $scope.homes = kami.getpath();
@@ -42,7 +39,32 @@ tpapp.controller('MainCtrl', function ($scope,kami,$http ) {
       $scope.persons = response.data;
     });
 
-console.log(" **** "+$scope.homes);
+    console.log(" **** "+$scope.homes);
 
+    $scope.openHomeAddForm = function(homeId) {
+console.log("modale ok past")
+      // Open a modal to show the selected user profile
+      var modal = Popeye.openModal({
+        templateUrl: "../views/home-form.tmpl.html",
+        resolve: {
+          user: function($http) {
+            return $http.get("/rest/home/" + homeId);
+          }
+        }
+      });
+
+      // Show a spinner while modal is resolving dependencies
+      $scope.showLoading = true;
+      modal.resolved.then(function() {
+        $scope.showLoading = false;
+      });
+
+      // Update user after modal is closed
+      modal.closed.then(function() {
+        $scope.showLoading = false;
+        //$scope.updateUser();
+      });
+    };
 
   });
+
